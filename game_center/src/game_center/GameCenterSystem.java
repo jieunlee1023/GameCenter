beeb937aaef440476cb1473b7e942df97392f1bf
IWBTM <rnlduadl2000@naver.com>
2022-10-20 17:42:13 +0900

    선생님의 명강의를 듣고 나서 pull 전..

--- game_center/src/game_center/GameCenterSystem.java
+++ game_center/src/game_center/GameCenterSystem.java
@@ -1,12 +1,8 @@
 package game_center;
 
-import game_center.service.GameCenterHostService;
-
 public class GameCenterSystem {
 
 	public static void main(String[] args) {
-
-		new GameCenterHostService();
 
 	}
 }
