dc30946ca8126d9138204c004a9e1311276d0a61
ji_euni <ji_euni@naver.com>
2022-10-20 10:06:49 +0900

    dev 데이터

--- game_center/src/game_center/view/GameCenterFrame.java
+++ game_center/src/game_center/view/GameCenterFrame.java
@@ -105,7 +105,7 @@ public class GameCenterFrame extends JFrame implements ActionListener {
 	private void addEventListener() {
 		join.addActionListener(this);
 		logIn.addActionListener(this);
-//		gameButton1.addActionListener(this);
+		gameButton1.addActionListener(this);
 		gameButton2.addActionListener(this);
 		gameButton3.addActionListener(this);
 		gameButton4.addActionListener(this);
@@ -115,14 +115,19 @@ public class GameCenterFrame extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 
 		JButton targetButton = (JButton) e.getSource();
+
 		if (targetButton.getText().equals(join.getText())) {
 			System.out.println("회원가입");
 		} else if (targetButton.getText().equals(logIn.getText())) {
 			System.out.println("로그인");
-		} else if (targetButton.getText().equals(gameButton1.getText())) {
+		} else if (targetButton.hashCode() == (gameButton1.hashCode())) {
 			System.out.println("게임 1");
-		} else if (targetButton.getText().equals(gameButton2.getText())) {
+		} else if (targetButton.hashCode() == (gameButton2.hashCode())) {
 			System.out.println("게임 2");
+		} else if (targetButton.hashCode() == (gameButton3.hashCode())) {
+			System.out.println("게임 3");
+		} else if (targetButton.hashCode() == (gameButton4.hashCode())) {
+			System.out.println("게임 4");
 		}
 	}
 
---
+++ game_center/src/game_center/view/JoinPage.java
@@ -0,0 +1,5 @@
+package game_center.view;
+
+public class JoinPage {
+
+}
