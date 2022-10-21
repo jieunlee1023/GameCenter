24007e3f707fce33d7de87a65a467361872a2469
ji_euni <ji_euni@naver.com>
2022-10-21 09:20:23 +0900

    전체 수정본

---
+++ game_center1/src/game_center/interfaces/IGameCenterHostService.java
@@ -0,0 +1,31 @@
+package game_center.interfaces;
+
+import game_center.dto.RequestGameCenter;
+
+public interface IGameCenterHostService extends IGameCenterService {
+
+	// 관리자의 인터페이스
+
+	int identityNum(String userId);
+
+	boolean insertGameCenter(String gameName); // 게임 등록
+
+	boolean insertGame(RequestGameCenter rgc); // 게임 등록
+
+	boolean insertChracter(RequestGameCenter rgc); // 게임 등록
+
+	boolean insertMap(RequestGameCenter rgc); // 게임 등록
+
+	void updateGame(RequestGameCenter rgc, String name); // 게임 정보 수정
+
+	void updateCharacter(RequestGameCenter rgc, String name); // 게임 캐릭터 수정
+
+	void updateMap(RequestGameCenter rgc, String name); // 게임 맵 수정
+
+	void deleteGame(String gameName); // 게임 삭제
+
+	void deleteCharater(String charaterName); // 게임 삭제
+
+	void deleteMap(String MapName); // 게임 삭제
+
+}
